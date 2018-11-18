051a8b65b641eacf5cfae92a000b129bb3f3a0e1
Nagenderr2Code <nagender2u@yahoo.co.in>
2018-11-18 12:38:42 -0500

    Implement Pet Clinic POJO Data Model. Closes #2

---
+++ src/main/java/guru/spring/springpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package guru.spring.springpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private Owner owner;
+    private  PetType petType;
+    private LocalDate dob;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+}
---
+++ src/main/java/guru/spring/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.spring.springpetclinic.model;
+
+public class Vet extends Person{
+}
