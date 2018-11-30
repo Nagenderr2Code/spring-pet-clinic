eada8310a8b0d554c4f452a60263ebcc1f7f4e00
Nagenderr2Code <nagender2u@yahoo.co.in>
2018-11-29 20:16:23 -0500

    Create Visit Map Service Closes #48

---
+++ pet-clinic-data/src/main/java/springpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,34 @@
+package springpetclinic.services.map;
+
+import springpetclinic.model.Visit;
+import springpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Long, Visit> implements VisitService {
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
