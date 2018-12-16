1fa77becf48516c261c765008587d1a86db535a5
Nagenderr2Code <nagender2u@yahoo.co.in>
2018-12-16 11:48:40 -0500

    Display Owner Record (HTML) Closes #55

--- pet-clinic-web/src/main/java/springpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/springpetclinic/controller/OwnerController.java
@@ -3,7 +3,10 @@ package springpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import springpetclinic.services.OwnerService;
 
 @Controller
@@ -26,6 +29,15 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable String ownerId){
+
+        ModelAndView mav= new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(Long.valueOf(ownerId)));
+
+        return mav;
     }
 
 }
